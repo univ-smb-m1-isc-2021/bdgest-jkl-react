d1f26c57009e984b549cf1110e7b9dc017f40412
Omar <omarkhial74@gmail.com>
2022-03-24 14:26:00 +0100

    testing api spring

--- spring-bd/src/main/java/com/manga/bdgest/HelloWorldControler.java
+++ spring-bd/src/main/java/com/manga/bdgest/HelloWorldControler.java
@@ -1,20 +1,28 @@
 package com.manga.bdgest;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HelloWorldControler {
 
-    @RequestMapping
-    public String helloWorld()
-    {
-        return "Hello World from Spring Boot";
+    @RequestMapping("/")
+    public ResponseEntity<JsonNode> get() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode json = mapper.readTree("{\"id\": \"132\", \"name\": \"Alice\"}");
+        return ResponseEntity.ok(json);
     }
 
-    @RequestMapping("/hoody")
-    public String hoody()
+    @RequestMapping("/hh")
+    public ResponseEntity<JsonNode> hello() throws JsonProcessingException
     {
-        return "Hoody yeah";
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode json = mapper.readTree("{\"hello\": \"world\"}");
+        return ResponseEntity.ok(json);
     }
 }
