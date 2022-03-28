ffaf8b2c0c0f36161582fc15f6c1a6d566b8309a
LuxemTheFez <37304100+LuxemTheFez@users.noreply.github.com>
2022-03-28 15:22:16 +0200

    Push Connexion Inscription

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './scss/App.scss';
 import 'bulma/css/bulma.min.css';
 import {MyComp} from './components/MyComp';
 import Connexion from './components/Connexion';
+import Inscription from './components/Inscription';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <header className="App-header">
         <MyComp/>
         <Connexion/>
+        <Inscription/>
         <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
