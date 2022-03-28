43471e564e69daec40f6663db856dc17e2562fcf
jubardt <dominiquejubard@gmail.com>
2022-03-28 16:55:40 +0200

    [UPDATE] component Album correcte

--- src/components/Album.js
+++ src/components/Album.js
@@ -7,32 +7,39 @@ import '../css/Album.css'
 }*/
 function Album(props) {
     return (
-        <article id="bodyAlbum" className='message is-success'>
-            <div class="message-header">
-                <p>Album</p>
-            </div>
-            <div class="message-body">
-                <div className='is-flex'>
-                    <img className='image' src={djam} alt="Placeholder image"/>
-                    <div>
-                        <h2 className='title'>Série</h2>
-                        <p className='subtitle'>{props.serie}</p>
-                        <h2 className='title'>Titre</h2>
-                        <p className='subtitle'>{props.title}</p>
-                        <h2 className='title'>Numéro</h2>
-                        <p className='subtitle'>{props.num}</p>
-                        <h2 className='title'>Auteur</h2>
-                        <p className='subtitle'>{props.author}</p>
-                        <h2 className='title'>ISBN</h2>
-                        <p className='subtitle'>{props.isbn}</p>
-                        <h2 className='title'>Date</h2>
-                        <p className='subtitle'>{props.date}</p>
-                        <h2 className='title'>Description</h2>
-                        <p className='subtitle'>{props.description}</p>
-                    </div>
+        <div id="bodyAlbum" className='box is-flex'>
+            <img  id='image' className='el' src={djam} alt="Placeholder image"/>
+            <div id='sideInformation' className='el'>
+                <div class="tags has-addons">
+                    <span class="tag is-dark is-medium">Série</span>
+                    <span class="tag is-info is-medium">{props.serie}</span>
+                </div>
+                <div class="tags has-addons">
+                    <span class="tag is-dark is-medium">Titre</span>
+                    <span class="tag is-info is-medium">{props.title}</span>
+                </div>
+                <div class="tags has-addons">
+                    <span class="tag is-dark is-medium">Numéro</span>
+                    <span class="tag is-info is-medium">{props.num}</span>
+                </div>
+                <div class="tags has-addons">
+                    <span class="tag is-dark is-medium">Auteur</span>
+                    <span class="tag is-info is-medium">{props.author}</span>
+                </div>
+                <div class="tags has-addons">
+                    <span class="tag is-dark is-medium">ISBN</span>
+                    <span class="tag is-info is-medium">{props.isbn}</span>
+                </div>
+                <div class="tags has-addons">
+                    <span class="tag is-dark is-medium">Date</span>
+                    <span class="tag is-info is-medium">{props.date}</span>
+                </div>
+                <div className='content'>
+                    <h2 className='tag is-info is-medium'>Description</h2>
+                    <blockquote id='descriptionAlbum'>{props.description}</blockquote>
                 </div>
             </div>
-        </article>
+        </div>
     )
 }
 export default Album;
