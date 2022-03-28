fd9b08c1827f5a64f6b2b33862db5b65e4542bc4
Omar <omarkhial74@gmail.com>
2022-03-28 16:13:09 +0200

    update dockerfile
    Co-authored-by: LuxemTheFez <LuxemTheFez@users.noreply.github.com>

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export default function HeaderComponent() {
+
+    return (
+        <div className="header">
+            <h1>ReactJS</h1>
+        </div>
+    );
+}
