4d73428d9276748ca1cbaceba4b167c8be140f0a
jubardt <dominiquejubard@gmail.com>
2022-03-28 17:51:11 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import 'bulma/css/bulma.min.css';
 import './css/App.css'
-import Album from './components/Album';
 import { Routes, Route } from "react-router-dom";
-import {MyComp} from './components/MyComp';
 import Connexion from './components/ConnexionComponent';
 import Inscription from './components/InscriptionComponent';
 import HomePage from './components/HomePage';
@@ -10,8 +8,6 @@ import HomePage from './components/HomePage';
 function App() {
   return (
     <div id='body'>
-        <Album serie="Marvel" title="Spiderman" isbn="2-7654-1005-7" num="269" author="Stan Lee" date="26/05/1988" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."/>
-      <h1>Welcome to React Router!</h1>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Connexion />} />
