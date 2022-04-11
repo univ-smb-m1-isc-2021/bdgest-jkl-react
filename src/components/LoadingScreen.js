c278c1cfed6d52fd013eb1ceedb974ae46ac1580
Omar <omarkhial74@gmail.com>
2022-04-11 02:03:25 +0200

    fix connexion

--- src/components/LoadingScreen.js
+++ src/components/LoadingScreen.js
@@ -7,12 +7,14 @@ const override = css`
   border-color: red;
 `;
 
+
+
 function LoadingScreen(props) {
     return (
         <>
             <div className="container is-fullhd is-fullheight">
                 <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
-                    <SquareLoader color={props.color} loading={props.loading} css={override} size={150} />
+                    <SquareLoader color={"#1448cd"} loading={props.loading} css={override} size={150} />
                 </div>
             </div>
         </>
